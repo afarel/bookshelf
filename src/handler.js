ff86bbd334f10f2484bcbb6f6a10c8843b448875
Rizky arrahman <riskiarahman2006@gmail.com>
2025-02-16 17:30:11 +0700

    add initial module exports and configuration files for nodemon and hapi

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,8 @@
 const {nanoId} = require('nanoid');
 
 const addBooksHandler = (request,h) =>{
+    const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+
+    
 
 }
