c081f720a3c2117335ec6b184c7f7ff46737f087
Rizky arrahman <riskiarahman2006@gmail.com>
2025-02-16 18:00:44 +0700

    add initial module exports and configuration files for nodemon and hapi

--- src/handler.js
+++ src/handler.js
@@ -2,7 +2,32 @@ const {nanoId} = require('nanoid');
 
 const addBooksHandler = (request,h) =>{
     const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
 
+    if(!name){
+        const response = h.response({
+            status: 'fail',
+            message: 'gagal menambahkan buku. Mohon isi nama buku '
+        })
+        response.code(400);
+        return response;
+    }
+
+    if (readPage > pageCount) {
+        const response = h.response({
+            status:fail,
+            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+        })
+        response.code(400);
+        return response;
+    }
+
+    return h.response({
+        status: 'success',
+        message: 'buku berhasil ditambahkan',
+        data:{ bookId: id,}
+    })
     
-
 }
