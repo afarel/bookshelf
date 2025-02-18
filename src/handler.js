de1b4de6d50002ef4ad03fc8e29446ef3f5ed32b
Rizky arrahman <riskiarahman2006@gmail.com>
2025-02-18 21:11:45 +0700

    implement book management handlers for adding, retrieving all, and retrieving by ID

--- src/handler.js
+++ src/handler.js
@@ -1,33 +1,89 @@
-const {nanoId} = require('nanoid');
+const { nanoId } = require('nanoid');
+const { books } = require('./books');
 
-const addBooksHandler = (request,h) =>{
+//  method post
+
+const addBooksHandler = (request,h) => {
     const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
-    const id = nanoid(16);
+    const id = nanoId(16);
     const insertedAt = new Date().toISOString();
     const updatedAt = insertedAt;
+    const finished = pageCount === readPage;
 
-    if(!name){
+    if (!name){
         const response = h.response({
             status: 'fail',
-            message: 'gagal menambahkan buku. Mohon isi nama buku '
-        })
-        response.code(400);
-        return response;
+            message: 'gagal menambahkan buku. Mohon isi nama buku ',
+        }).code(400);
     }
 
     if (readPage > pageCount) {
-        const response = h.response({
-            status:fail,
+        return h.response({
+            status: 'fail',
             message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
-        })
-        response.code(400);
-        return response;
+        }).code(400);
     }
+
+    const newBook = {
+        id,
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        finished,
+        reading,
+        insertedAt,
+        updatedAt,
+    };
+    books.push(newBook);
 
     return h.response({
         status: 'success',
         message: 'buku berhasil ditambahkan',
-        data:{ bookId: id,}
-    })
-    
-}
+        data: { bookId: id },
+    }).code(201); 
+};
+
+
+const getAllBooksHandler = (request, h) => {
+    const booksResponse = books.map((book)=> ({
+        id: book.id,
+        name: book.name,
+        publisher: book.publisher,
+    }));
+
+    return h.response({
+        status: 'success',
+        data: {
+            books: booksResponse
+        }
+    }).code(200);
+};
+
+const getBooksByIdHandler = (request, h) => {
+    const { id } = request.params;
+    const book = books.find(n => n.id === id);
+
+    if (book !== undefined) {
+        return h.respponse ({
+            status: 'success',
+            data: {
+                book,
+            },
+        }).code(200);
+    }
+
+    return h.response ({
+        status: 'fail',
+        message: 'Buku tidak ditemukan',
+    }).code(404);
+};
+
+module.exports = {
+    addBooksHandler,
+    getAllBooksHandler,
+    getBooksByIdHandler,
+};
